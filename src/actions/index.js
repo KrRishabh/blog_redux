a850fdea32fab848808c3174b830391975165cc2
Kumar Rishabh <kr.rishabh72@gmail.com>
2021-10-07 03:35:07 +0530

    final commit

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,17 @@
 import JsonPlaceholder from "../apis/JsonPlaceholder";
+import _ from "lodash";
 
 export const fetchPost = () => async (dispatch) => {
   const response = await JsonPlaceholder.get("/posts");
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
-export const fetchUser = (id) => async (dispatch) => {
+export const fetchUser = (id) => (dispatch) => {
+  _fetchUser(id, dispatch);
+};
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
   const response = await JsonPlaceholder.get("/users/" + id);
   //   console.log(response.data);
   dispatch({ type: "FETCH_USER", payload: response.data });
-};
+});
